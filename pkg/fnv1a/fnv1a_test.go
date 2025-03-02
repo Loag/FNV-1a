540cf623294ebd6451e02aa495eb236f74dd604b
Logan <10239411+Loag@users.noreply.github.com>
2025-03-02 11:22:36 -0500

    add benchs for all implementations

--- pkg/fnv1a/fnv1a_test.go
+++ pkg/fnv1a/fnv1a_test.go
@@ -27,10 +27,73 @@ func TestHash32(t *testing.T) {
 	}
 }
 
+func TestHash64(t *testing.T) {
+	res, _ := hex.DecodeString("a430d84680aabd0b")
+
+	tests := []struct {
+		input    []byte
+		expected []byte
+	}{
+		{[]byte("hello"), res},
+	}
+
+	hasher := NewFNV1a64()
+
+	for _, tt := range tests {
+		result := hasher.Hash(tt.input)
+
+		if !bytes.Equal(result, tt.expected) {
+			t.Errorf("Hash(%q) = %v; want %v", tt.input, result, tt.expected)
+		}
+	}
+}
+
+func BenchmarkHash32(b *testing.B) {
+	data := []byte("Hello")
+	hasher := NewFNV1a32()
+
+	for b.Loop() {
+		hasher.Hash(data)
+	}
+}
+
 func BenchmarkHash64(b *testing.B) {
 	data := []byte("Hello")
 	hasher := NewFNV1a64()
 
+	for b.Loop() {
+		hasher.Hash(data)
+	}
+}
+
+func BenchmarkHash128(b *testing.B) {
+	data := []byte("Hello")
+	hasher := NewFNV1a128()
+
+	for b.Loop() {
+		hasher.Hash(data)
+	}
+}
+
+func BenchmarkHash256(b *testing.B) {
+	data := []byte("Hello")
+	hasher := NewFNV1a256()
+	for b.Loop() {
+		hasher.Hash(data)
+	}
+}
+
+func BenchmarkHash512(b *testing.B) {
+	data := []byte("Hello")
+	hasher := NewFNV1a512()
+	for b.Loop() {
+		hasher.Hash(data)
+	}
+}
+
+func BenchmarkHash1024(b *testing.B) {
+	data := []byte("Hello")
+	hasher := NewFNV1a1024()
 	for b.Loop() {
 		hasher.Hash(data)
 	}
